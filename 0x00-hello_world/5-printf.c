56fcdbcb89fed6157ad8a8a95a5f9d30bbf06bf6
Frank-Cisca <consultcisca@gmail.com>
2023-06-15 20:21:42 -0700

    corrected these files

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-             puts("with proper grammar, but the outcome is a piece of art,\n");
-             return (0);
+
+int main() {
+    printf("Art is the lie that enables us to realize the truth,\n");
+    printf("but the outcome is a piece of art.\n");
+
+    return 0;
 }
